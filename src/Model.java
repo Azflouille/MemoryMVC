ca2efb9cc24506522897235b06f976646e2c232d
Azflouille <90977486+Azflouille@users.noreply.github.com>
2021-11-04 15:45:26 +0100

    Le Modele

---
+++ src/Model.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Model implements Sujet{
+
+    private ArrayList<Carte> cartes;
+    private ArrayList<Observateur> observateurs;
+    private Carte cartePrecedente;
+    private int NbPaires;
+    private int NbCoups;
+
+    @Override
+    public void enregistrerObservateur(Observateur o) {
+        this.observateurs.add(o);
+    }
+
+    @Override
+    public void supprimerObservateur(Observateur o) {
+        int i = this.observateurs.indexOf(o);
+        if (i >= 0) {
+            this.observateurs.remove(i);
+        }
+    }
+
+    @Override
+    public void notifierObservateurs() {
+        for (int i = 0; i < this.observateurs.size(); i++) {
+            Observateur observer = this.observateurs.get(i);
+            observer.actualiser(this);
+        }
+    }
+}
